c0483ea405f750c9214e89f7f5275f6b7d6de1f1
Paulo Angelo <pa@pauloangelo.com>
2025-11-14 18:49:48 -0300

    bugs

--- test/test_script.php
+++ test/test_script.php
@@ -1,23 +1,6 @@
 <?php
+    $file = "login.php";
 
-function plugin_guardiankeyauth_install() {
-    global $DB;
-
-    $query = "CREATE TABLE IF NOT EXISTS `glpi_plugin_guardiankeyauth_configs` (
-        `id` INT AUTO_INCREMENT PRIMARY KEY,
-        `orgid` VARCHAR(64),
-        `authgroupid` VARCHAR(64),
-        `key` TEXT,
-        `iv` TEXT
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
-    
-    $DB->query($query);
-
-    // Insert config default (single line table)
-    $DB->query("INSERT INTO `glpi_plugin_guardiankeyauth_configs` (`orgid`, `authgroupid`, `key`, `iv`)
-                VALUES ('', '', '', '')");
-
-    $file = GLPI_ROOT . "/front/login.php";
 
    // Verify if exists GK_BEGIN_BLOCK in file, to avoid duplicate insertions
    $content_check = file_get_contents($file);
@@ -68,24 +51,4 @@ function plugin_guardiankeyauth_install() {
 
       file_put_contents($file, $content);
     }
-    return true;
-}
-
-function plugin_guardiankeyauth_uninstall() {
-   global $DB;
-   $DB->query("DROP TABLE IF EXISTS `glpi_plugin_guardiankeyauth_configs`");
-
-   $file = GLPI_ROOT . "/front/login.php";
-
-   if (is_writable($file) && file_exists($file)) {
-      $content = file_get_contents($file);
-      $content = preg_replace(
-      '|// GK_BEGIN_BLOCK(.*?)// GK_END_BLOCK|si', 
-      '', 
-      $content
-      );
-      file_put_contents($file, $content);
-   }
-    return true;
-}
 
